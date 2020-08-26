7b60b28a1898639e05faeb02797047232d8af9c8
Gonzo <josgonzalopg@gmail.com>
2020-08-26 02:58:56 -0600

    Graphs in Cairo

--- Utils/pruebas.c
+++ Utils/pruebas.c
@@ -2,48 +2,60 @@
 #include <stdlib.h>
 #include <math.h>
 
-double **fill_square_matrix(){
-    static int row;
-    
-    printf("Numero de filas/columnas: " );
-    scanf("%d", &row);
-    printf("\n");
+#define ndata 3
 
-    double **matrix;
-    matrix = malloc(row*sizeof(double*));
-    
-    for(int i = 0; i < row; i++) {
-        matrix[i] = malloc(row*sizeof(double*));
-    }
-
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < row; j++){
-            printf("Fila: %d, Columna: %d \n", i, j);
-            scanf("%lf", &matrix[i][j]);
-        }
-    }
-
-    return matrix;
+// Función de prueba
+double function(double x){
+    return pow(x,2) + 10;
 }
 
-void print_matrix(double **matrix){
-    int row, col;
-    row = 3;
-    col = 3;
+// Generación de  los puntos yi
+void y_tabular(double *x, double *y){
 
-    for(int i = 0; i < row; i++){
-            for(int j = 0; j < row; j++){
-                printf("%0.2ld   ", matrix[i][j]);
-            }
-        printf("\n");
-        }
-    
+    for(int i = 0; i < ndata; i++){
+        y[i] = function(x[i]);
+    }
+
     return;
 }
 
-int main(){
-    double **take;
+void double2char(double *x, char *labels){
 
-    take = fill_square_matrix();
-    print_matrix(take);
+    for(int i = 0; i < ndata; i++){
+        labels[i] = (char)x[i];
+    }
+    return;
+}
+
+double max_value(double *y){
+    
+    double max_value = y[0];
+
+    // Se recorre el arreglo desde el segundo elemento y
+    // se compara cada elemento con el valor máximo actual
+    for(int i = 1; i < ndata; i++){
+        if(y[i] > max_value){
+            max_value = y[i];
+        }
+    }
+
+    return max_value;
+}
+int main(){
+    double x = 2.01;
+    char result[ndata][10];
+
+    for(int i = 0; i < ndata; i++){
+        sprintf(result[i], "%0.3lf", x);
+    }
+
+    for(int i = 0; i < ndata; i++){
+        printf("Numero %s \n", result[i]);
+    }
+
+    double delta_x = (2 - 1)/ 
+}
+
+double line_equation_y(double axis, double max_val, double vector){
+    return vector*axis/max_val;
 }
